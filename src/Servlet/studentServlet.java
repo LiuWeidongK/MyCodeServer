0ac292e7e7b96909a11bb39aa59c2398da3bd251
LiuWeidon <liu18443101163@163.com>
2016-11-13 20:17:40 +0800

    2016-11-13 20:17

--- src/Servlet/studentServlet.java
+++ src/Servlet/studentServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import Class.studentSolve;
 
@@ -13,12 +14,26 @@ public class studentServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=utf-8");
 
-        String jsonStr = request.getParameter("json");
+        BufferedReader bufferedReader = request.getReader();
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null){
+            stringBuilder.append(line);
+        }
+        String jsonStr = stringBuilder.toString();
         System.out.println(jsonStr);
         studentSolve solve = new studentSolve();
         Boolean sign = solve.solve(jsonStr);
         System.out.println(sign);
-        response.getOutputStream().write("ok".getBytes());
+
+        response.getWriter().write("ok");
+
+        /*String jsonStr = request.getParameter("json");
+        System.out.println(jsonStr);
+        studentSolve solve = new studentSolve();
+        Boolean sign = solve.solve(jsonStr);
+        System.out.println(sign);
+        response.getOutputStream().write("ok".getBytes());*/
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- web/index.jsp
+++ web/index.jsp
@@ -1,9 +1,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>Index JSp Page</title>
-  </head>
-  <body>
+<head>
+  <title>Index JSp Page</title>
+</head>
+<body>
 
-  </body>
+</body>
 </html>
